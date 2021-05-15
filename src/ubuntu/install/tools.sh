a833b87a241ae2779ac05601689eae4c5e809435
Paul <mail.paul.perie@gmail.com>
2021-05-16 01:15:33 +0200

    [Feature] Set French locale

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -8,5 +8,17 @@ apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy sudo cron #used for websockify/novnc
 apt-get clean -y
 
-echo "generate locales für en_US.UTF-8"
-locale-gen en_US.UTF-8
+#echo "generate locales für en_US.UTF-8"
+#locale-gen en_US.UTF-8
+
+# Set the locale
+#RUN apt update
+#RUN apt install -y locales
+#RUN touch /usr/share/locale/locale.alias
+#ENV LANG fr_FR.UTF-8  
+#ENV LANGUAGE fr_FR:fr  
+#ENV LC_ALL fr_FR.UTF-8 
+#RUN locale-gen fr_FR.UTF-8  
+
+#echo "generate locales for fr_FR.UTF-8"
+#locale-gen fr_FR.UTF-8
--- src/ubuntu/install/xfce_ui.sh
+++ src/ubuntu/install/xfce_ui.sh
@@ -2,8 +2,13 @@
 ### every exit != 0 fails the script
 set -e
 
+# set locale fr
+apt-get update
+apt-get install -y locales language-pack-fr
+locale-gen fr_FR.UTF-8
+update-locale LANG=fr_FR.UTF-8
+
 echo "Install Xfce4 UI components"
-apt-get update 
 apt-get install -y supervisor xfce4 xfce4-terminal xterm
 apt-get purge -y pm-utils xscreensaver*
 apt-get clean -y
