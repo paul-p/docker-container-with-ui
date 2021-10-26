62918df659211918f52216ff6451908a0dcfbe9e
Paul <mail.paul.perie@gmail.com>
2021-10-26 23:48:52 +0200

    [Fix] Fix installation of SSH server
    If the sourcelist is not the original some dependencies are missing

--- src/ubuntu/install/ssh.sh
+++ src/ubuntu/install/ssh.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
+apt-get update
 apt-get install -y ssh
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
