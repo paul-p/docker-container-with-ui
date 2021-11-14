f4873a68806ff83f786bb5a58549fe9991854644
Paul <mail.paul.perie@gmail.com>
2021-11-14 16:45:33 +0100

    [Feature] Improve again the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,12 @@ VIRTUAL_HOST_INSTALL=`dirname "$0"`
 VIRTUAL_HOST_IMAGE_NAME="virtuel_host_image"
 VIRTUAL_HOST_CONTAINER_NAME="virtual_host_container"
 echo "Que souhaitez vos faire ?"
-echo "[1] Désinstaller le poste virtuel (arrêt du container et suppression de l'image docker) (y/n)" 
-echo "[2] Installer le poste virtuel (création de l'image docker et lancement du container) (y/n)"
-echo "[3] Stopper le poste virtuel (y/n)"
-echo "[4] Lancer le poste virtuel (y/n)"
+echo "[1] Lancer le poste virtuel (création de l'image docker et lancement du container)"
+echo "[2] Stopper le poste virtuel"
+echo "[3] Désinstaller le poste virtuel (arrêt du container et suppression de l'image docker)" 
 read -p ">" ACTION
 
-
-if [ "$ACTION" = "1" ] || [ "$ACTION" = "2" ] || [ "$ACTION" = "3" ] || [ "$ACTION" = "4" ]; then
+if [ "$ACTION" = "1" ] || [ "$ACTION" = "2" ] || [ "$ACTION" = "3" ]; then
     # Ask sudo rights
     sudo ls > /dev/null
     echo "Arrêt d'un éventuel poste virtuel antérieur..."
@@ -22,11 +20,6 @@ if [ "$ACTION" = "1" ] || [ "$ACTION" = "2" ] || [ "$ACTION" = "3" ] || [ "$ACTI
 fi
 
 if [ "$ACTION" = "1" ]; then
-    echo "Suppression de l'image docker"
-    sudo docker image rm $VIRTUAL_HOST_IMAGE_NAME
-fi
-
-if [ "$ACTION" = "2" ]; then
     read -p "Entrez le login de l'utilisateur sur le poste virtuel " GUEST_USERNAME  
     read -p "Entrez le mot de passe de l'utilisateur sur le poste virtuel   " GUEST_PASSWORD  
 
@@ -36,11 +29,10 @@ if [ "$ACTION" = "2" ]; then
     docker run -d --name $VIRTUAL_HOST_CONTAINER_NAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged --expose 22 $VIRTUAL_HOST_IMAGE_NAME
     echo "Ajout du compte utilisateur..."s
     docker exec -t $VIRTUAL_HOST_CONTAINER_NAME /dockerstartup/createUser.sh $GUEST_USERNAME $GUEST_PASSWORD
-    echo "Le poste virtuel est maintenant démarré et expose un port SSH sur cette adresse: $(docker inspect $VIRTUAL_HOST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}'):22"
+    echo "Le poste virtuel est maintenant démarré. Vous pouvez y accéder avec ssh : ssh $GUEST_USERNAME@$(docker inspect $VIRTUAL_HOST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}')"
 fi
 
-if [ "$ACTION" = "4" ]; then
-    echo "Démarrage du poste virtuel"
-    docker run -d --name $VIRTUAL_HOST_CONTAINER_NAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged --expose 22 $VIRTUAL_HOST_IMAGE_NAME
-    echo "Le poste virtuel est maintenant démarré et expose un port SSH sur cette adresse: $(docker inspect $VIRTUAL_HOST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}'):22"
+if [ "$ACTION" = "3" ]; then
+    echo "Suppression de l'image docker"
+    sudo docker image rm $VIRTUAL_HOST_IMAGE_NAME
 fi
