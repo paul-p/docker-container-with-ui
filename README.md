8fb48916186977d071c3d88b6dfe75c2d24f0af0
Paul <mail.paul.perie@gmail.com>
2021-11-14 16:11:10 +0100

    [Refactor] Remove ansible dependencie, improve the bootstrap script

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ It contains
 * ubuntu:18.04
 * UI components (XFCE)
 * Ligthdm links (the virtual host will be accessible directly on a Xorg session). You should be able to swith between your host session and your container session with Ctrl-F2 and Ctrl-F7
-* Docker links (to be able to do Docker in Docker in your virtual host)
+
 * Firefox in french
 * An SSH serveur which accepts authentication by password
 
@@ -18,13 +18,29 @@ It contains
 On ubuntu 18.04
 
 ## How to run it
-Just clone the project and start the boostrap.sh script
+First, you need to install docker
 ```
-sudo git clone -b main https://github.com/paul-p/docker-headless-vnc-container.git
+sudo apt-get install docker.io
+```
+Then, just clone the project and start the boostrap.sh script
+```
+git clone -b main https://github.com/paul-p/docker-headless-vnc-container.git
 cd docker-headless-vnc-container.git
 ./bootstrap.sh
 ```
 
 You will have to answer few questions
-* Uninstall or Install ?
-* Login and Password for creation of a new user
+```
+Que souhaitez vos faire ?
+[1] Désinstaller le poste virtuel (arrêt du container et suppression de l'image docker) (y/n)
+[2] Installer le poste virtuel (création de l'image docker et lancement du container) (y/n)
+[3] Stopper le poste virtuel (y/n)
+[4] Lancer le poste virtuel (y/n) 
+>
+```
+If you install, some other questions will be asked
+```
+Entrez le login de l'utilisateur sur le poste virtuel
+Entrez le mot de passe de l'utilisateur sur le poste virtuel
+```
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,44 +2,45 @@
 # A bash script to install a docker container with UI component and lightdm service
 set +e
 
-VIRTUAL_GUEST_INSTALL=`dirname "$0"`
-VIRTUAL_GUEST_IMAGE_NAME="clap_virtual_guest_image"
-VIRTUAL_GUEST_CONTAINER_NAME="virtual_host_container"
+VIRTUAL_HOST_INSTALL=`dirname "$0"`
+VIRTUAL_HOST_IMAGE_NAME="virtuel_host_image"
+VIRTUAL_HOST_CONTAINER_NAME="virtual_host_container"
+echo "Que souhaitez vos faire ?"
+echo "[1] Désinstaller le poste virtuel (arrêt du container et suppression de l'image docker) (y/n)" 
+echo "[2] Installer le poste virtuel (création de l'image docker et lancement du container) (y/n)"
+echo "[3] Stopper le poste virtuel (y/n)"
+echo "[4] Lancer le poste virtuel (y/n)"
+read -p ">" ACTION
 
-read -p "Voulez vous désinstaller le poste virtuel (y/n) " UNINSTALLVIRTUALGUEST  
-read -p "Voulez vous installer / re-installer le poste virtuel (Xubuntu dockerisée) (y/n) " VIRTUALGUEST
 
-if [ "$UNINSTALLVIRTUALGUEST" = "y" ]; then
+if [ "$ACTION" = "1" ] || [ "$ACTION" = "2" ] || [ "$ACTION" = "3" ] || [ "$ACTION" = "4" ]; then
     # Ask sudo rights
     sudo ls > /dev/null
-    echo "Suppression d'un éventuel poste virtuel antérieur..."
-    sudo docker stop $VIRTUAL_GUEST_CONTAINER_NAME
-    sudo docker rm $VIRTUAL_GUEST_CONTAINER_NAME
-    sudo docker image rm $VIRTUAL_GUEST_IMAGE_NAME
+    echo "Arrêt d'un éventuel poste virtuel antérieur..."
+    sudo docker stop $VIRTUAL_HOST_CONTAINER_NAME
+    sudo docker rm $VIRTUAL_HOST_CONTAINER_NAME
 fi
 
-if [ "$VIRTUALGUEST" = "y" ]; then
-    echo "Installation des dépendances..."
+if [ "$ACTION" = "1" ]; then
+    echo "Suppression de l'image docker"
+    sudo docker image rm $VIRTUAL_HOST_IMAGE_NAME
+fi
 
-    # Ask sudo rights
-    sudo ls > /dev/null
+if [ "$ACTION" = "2" ]; then
+    read -p "Entrez le login de l'utilisateur sur le poste virtuel " GUEST_USERNAME  
+    read -p "Entrez le mot de passe de l'utilisateur sur le poste virtuel   " GUEST_PASSWORD  
 
-    # Install ansible
-    sudo dpkg -s ansible &> /dev/null
-    if [ $? -ne 0 ]; then
-        sudo apt-add-repository --yes --update ppa:ansible/ansible
-        sudo apt-get install -y ansible
-    fi
+    echo "Construction de l'image ..."
+    docker build -t $VIRTUAL_HOST_IMAGE_NAME .
+    echo "Démarrage du poste virtuel..."
+    docker run -d --name $VIRTUAL_HOST_CONTAINER_NAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro --expose 22 $VIRTUAL_HOST_IMAGE_NAME
+    echo "Ajout du compte utilisateur..."s
+    docker exec -t $VIRTUAL_HOST_CONTAINER_NAME /dockerstartup/createUser.sh $GUEST_USERNAME $GUEST_PASSWORD
+    echo "Le poste virtuel est maintenant démarré et expose un port SSH sur cette adresse: $(docker inspect $VIRTUAL_HOST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}'):22"
+fi
 
-    read -p "Entrez le login de l'utilisateur sur le poste invité virtuel " GUEST_USERNAME  
-    read -p "Entrez le mot de passe de l'utilisateur sur le poste invité virtuel   " GUEST_PASSWORD  
-
-    echo "Arrêt d'un éventuel poste virtuel antérieur..."
-    sudo docker stop $VIRTUAL_GUEST_CONTAINER_NAME
-    #sudo docker rm $VIRTUAL_GUEST_CONTAINER_NAME
-
-    echo "Installation du poste virtuel ..."
-    sudo ansible-playbook -K ./playbook.yml --extra-vars "GUEST_USERNAME=$GUEST_USERNAME set GUEST_PASSWORD=$GUEST_PASSWORD set VIRTUAL_GUEST_INSTALL=$VIRTUAL_GUEST_INSTALL set VIRTUAL_GUEST_CONTAINER_NAME=$VIRTUAL_GUEST_CONTAINER_NAME set VIRTUAL_GUEST_IMAGE_NAME=$VIRTUAL_GUEST_IMAGE_NAME"
-    
-    echo "Le poste invité virtuel est maintenant installé et expose un port SSH sur cette adresse: $(docker inspect $VIRTUAL_GUEST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}'):22"
+if [ "$ACTION" = "4" ]; then
+    echo "Démarrage du poste virtuel"
+    docker run -d --name $VIRTUAL_HOST_CONTAINER_NAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro --expose 22 $VIRTUAL_HOST_IMAGE_NAME
+    echo "Le poste virtuel est maintenant démarré et expose un port SSH sur cette adresse: $(docker inspect $VIRTUAL_HOST_CONTAINER_NAME --format '{{.NetworkSettings.Networks.bridge.IPAddress}}'):22"
 fi
